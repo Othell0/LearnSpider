4a8e0e56e173cced3da8ef407bd1d5a5f4da2beb
othell0 <othell0@outlook.com>
2018-08-15 17:37:12 +0800

    Add download.

--- download.py
+++ download.py
@@ -2,11 +2,9 @@ import os
 
 import requests
 
-os.makedirs('./img/', exist_ok=True)
-
 IMAGE_URL = "https://morvanzhou.github.io/static/img/description/learning_step_flowchart.png"
-
 r = requests.get(IMAGE_URL, stream=True)  # stream loading
+os.makedirs('./img/', exist_ok=True)
 
 with open('./img/image3.png', 'wb') as f:
     for chunk in r.iter_content(chunk_size=32):
